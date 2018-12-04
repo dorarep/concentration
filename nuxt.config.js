e47b6e5d98f38cac393e214a341a48ad74a8e552
dorarep <ishikami.naotaka@gmail.com>
2018-12-05 02:51:21 +0900

    disable scale.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'concentration',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0' },
       { hid: 'description', name: 'description', content: 'Concenstration app' }
     ],
     link: [
