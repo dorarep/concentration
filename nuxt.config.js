921020f0344ba3aa91f56e8f642a269ec314efcc
dorarep <ishikami.naotaka@gmail.com>
2018-12-05 03:07:47 +0900

    set user-scalable no.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'concentration',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'Concenstration app' }
     ],
     link: [
