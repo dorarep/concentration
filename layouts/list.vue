e1580cba8046d08989f14ca09d6c14f92423ccb5
dorarep <ishikami.naotaka@gmail.com>
2018-12-05 03:12:34 +0900

    disable scroll-x.

--- layouts/list.vue
+++ layouts/list.vue
@@ -3,22 +3,6 @@
 </template>
 
 <style>
-html {
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-  font-family: 'Roboto', sans-serif;
-}
-
-a, a:hover, u {
-  text-decoration: none;
-  color: white;
-}
-
-html, body {margin: 0; height: 100%; overflow: scroll}
+  html, body {margin: 0; height: 100%; overflow-y: scroll}
 </style>
 
